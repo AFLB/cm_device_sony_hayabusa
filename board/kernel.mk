65e9a63fffb2d9056d5f2a873dc07dc75708391e
5x <droidfivex@gmail.com>
2016-12-21 02:22:43 +0900

    hayabusa: add device custombootimg.mk
    add prebuilt kernel from 9.2.A.1.215
    
    Change-Id: I69a5b9a1a13885ad0cf29816c758a756c0eab878

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,2 +1,6 @@
 # Kernel properties
 TARGET_KERNEL_CONFIG := blue_hayabusa_defconfig
+
+# Custom boot
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/boot/custombootimg.mk
---
+++ boot/custombootimg.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+HIJACK_PATH := $(PRODUCT_OUT)/system/hijack
+
+INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
+$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/busybox
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(MKBOOTFS) $(TARGET_ROOT_OUT) > $(HIJACK_PATH)/ramdisk.cpio
+	$(hide) cp $(PRODUCT_OUT)/utilities/busybox $(HIJACK_PATH)/
+	$(hide) rm $(INSTALLED_BOOTIMAGE_TARGET)
+	$(hide) cp $(LOCAL_PATH)/boot.elf $(INSTALLED_BOOTIMAGE_TARGET)
+
+INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel)
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) cp $(recovery_ramdisk) $(HIJACK_PATH)/
