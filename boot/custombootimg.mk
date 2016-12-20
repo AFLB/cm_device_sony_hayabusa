b2b03247ea1b1d7ef596b401e468808820eaee44
5x <droidfivex@gmail.com>
2016-12-20 18:11:31 +0000

    hayabusa: boot: modify for alone build
    Change-Id: I3c195a5d6f8de5d92e3a7a93e8aa17abf66fee4d

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -3,14 +3,18 @@ LOCAL_PATH := $(call my-dir)
 HIJACK_PATH := $(PRODUCT_OUT)/system/hijack
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/busybox
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel \
+		$(INSTALLED_RAMDISK_TARGET) \
+		$(PRODUCT_OUT)/utilities/busybox
+	@echo -e ${CL_CYN}"----- Optimizing boot ramdisk ------"${CL_RST}
+	$(hide) mkdir -p $(HIJACK_PATH)
 	$(hide) $(MKBOOTFS) $(TARGET_ROOT_OUT) > $(HIJACK_PATH)/ramdisk.cpio
 	$(hide) cp $(PRODUCT_OUT)/utilities/busybox $(HIJACK_PATH)/
-	$(hide) rm $(INSTALLED_BOOTIMAGE_TARGET)
+	@echo -e ${CL_CYN}"----- Copy prebuilt stock kernel ------"${CL_RST}
 	$(hide) cp $(LOCAL_PATH)/boot.elf $(INSTALLED_BOOTIMAGE_TARGET)
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel)
-	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk) $(recovery_kernel)
+	@echo -e ${CL_CYN} "----- Optimizing recovery ramdisk ------"${CL_RST}
+	$(hide) mkdir -p $(HIJACK_PATH)
 	$(hide) cp $(recovery_ramdisk) $(HIJACK_PATH)/
